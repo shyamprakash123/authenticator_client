be36ea50b1e90ef7638ff8c051fdab946244bfaf
Shyam prakash <shyamprakash9959@gmail.com>
2024-05-17 11:56:37 +0530

    implemented client side features

--- pages/2fa.js
+++ pages/2fa.js
@@ -1,52 +1,102 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const TwoFactorAuth = () => {
-    const [code, setCode] = useState('');
-    const router = useRouter();
+  const [code, setCode] = useState("");
+  const [userId, setUserId] = useState(null);
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('/api/2fa', { code });
-            if (response.data.success) {
-                // Redirect user to dashboard or another authenticated page
-                router.push(response.data.redirect);
-            } else {
-                console.error('2FA failed:', response.data.message);
-            }
-        } catch (error) {
-            console.error('2FA failed:', error);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userId = localStorage.getItem("userId");
+      setUserId(userId);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (userId !== null) {
+        const response = await fetch(
+          "https://authenticator-kappa.vercel.app/api/verifycode",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              code,
+              userId: parseInt(userId),
+              clientSecret: "4ddf22d2-253d-47f5-9089-199f7c7c483c",
+            }),
+          }
+        );
+        const data = await response.json();
+        if (response.ok) {
+          toast.success(data.msg);
+          setTimeout(() => {
+            router.push("/dashboard");
+          }, 1500);
+        } else {
+          toast.error(data.error);
+          console.error("2FA failed:", data.error);
         }
-    };
+      } else {
+        toast.error("User not found, please signup first.");
+      }
+    } catch (error) {
+      console.error("2FA failed:", error);
+    }
+  };
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Two-Factor Authentication</h2>
-
-                </div>
-                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    <div className="rounded-md shadow-sm space-y-3">
-                        <h2 className='text-center text-md text-gray-400'>Enter verification code received in your My Authenticator App</h2>
-                        <div>
-
-                            <label htmlFor="code" className="sr-only">Code</label>
-                            <input id="code" name="code" type="text" autoComplete="code" required className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter code" value={code} onChange={(e) => setCode(e.target.value)} />
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Verify
-                        </button>
-                    </div>
-                </form>
+  return (
+    <div>
+      <ToastContainer />
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Two-Factor Authentications{userId}
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <div className="rounded-md shadow-sm space-y-3">
+              <h2 className="text-center text-md text-gray-400">
+                Enter verification code received in your My Authenticator App
+              </h2>
+              <div>
+                <label htmlFor="code" className="sr-only">
+                  Code
+                </label>
+                <input
+                  id="code"
+                  name="code"
+                  type="text"
+                  minLength={6}
+                  required
+                  className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter code"
+                  value={code}
+                  onChange={(e) => setCode(e.target.value)}
+                />
+              </div>
             </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Verify
+              </button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TwoFactorAuth;
--- pages/signup.js
+++ pages/signup.js
@@ -1,62 +1,160 @@
-import { useState } from 'react';
-import axios from 'axios';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignUp = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            // Send user registration data to the server
-            const response = await axios.post('/api/signup', { username, password });
-            // Handle successful registration (redirect, show message, etc.)
-            console.log('Registration successful:', response.data);
-        } catch (error) {
-            console.error('Registration failed:', error);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [authToken, setAuthToken] = useState("");
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://authenticator-kappa.vercel.app/api/link",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            authToken,
+            clientSecret: "4ddf22d2-253d-47f5-9089-199f7c7c483c",
+          }),
         }
-    };
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        toast.error(data.error);
+      } else {
+        localStorage.setItem("userId", data.userId);
+        toast.success("Registration successful");
+        setTimeout(() => {
+          router.push("/login");
+        }, 1500);
+      }
+    } catch (error) {
+      console.error("Registration failed:", error);
+    }
+  };
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create an account</h2>
-                </div>
-                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="username" className="sr-only">Username</label>
-                        <input id="username" name="username" type="text" autoComplete="username" required className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="Email" className="sr-only">Email</label>
-                        <input id="Email" name="Email" type="text" autoComplete="username" required className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="password" className="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autoComplete="new-password" required className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div>
-                        <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Sign up
-                        </button>
-                    </div>
-                </form>
-                <div className="text-center">
-                    <p className="mt-2 text-sm text-gray-600">Already have an account? <Link href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">Sign in</Link></p>
-                </div>
-                <div className='text-sm text-gray-600'>
-                    <p>Dear User,</p>
-                    <span>To complete the registration process: </span>
-                    <span>   <a href="https://example.com/authenticator-registration?username=USERNAME&email=USER_EMAIL&code=REGISTRATION_CODE" className='text-blue-500'> Register in Authenticator App</a></span>
+  return (
+    <div>
+      <ToastContainer />
 
-                </div>
-
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Create an account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
+            <div>
+              <label htmlFor="Email" className="sr-only">
+                Email
+              </label>
+              <input
+                id="Email"
+                name="Email"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="authToken" className="sr-only">
+                Auth Token
+              </label>
+              <input
+                id="authToken"
+                name="authToken"
+                type="text"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Enter Auth Token"
+                value={authToken}
+                onChange={(e) => setAuthToken(e.target.value)}
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+          <div className="text-center">
+            <p className="mt-2 text-sm text-gray-600">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+          <div className="text-sm text-gray-600">
+            <p>Dear User,</p>
+            <span>To complete the registration process: </span>
+            <span>
+              {" "}
+              <a
+                href="https://authenticator-kappa.vercel.app/"
+                target="_blank"
+                className="text-blue-500"
+              >
+                {" "}
+                Register in Authenticator App
+              </a>
+            </span>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
